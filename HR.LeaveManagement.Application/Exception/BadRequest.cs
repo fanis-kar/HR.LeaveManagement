b177bed901d07ff364bdf86a7fa8d04f7841fefa
Theofanis Karamichalelis <fanis.karamichalelis@gmail.com>
2025-01-19 01:16:07 +0200

    Add Fluent Validation

--- HR.LeaveManagement.Application/Exception/BadRequest.cs
+++
@@ -1,8 +0,0 @@
-﻿namespace HR.LeaveManagement.Application.Exception;
-
-public class BadRequest : System.Exception
-{
-    public BadRequest(string message) : base(message)
-    {
-    }
-}
---
+++ HR.LeaveManagement.Application/Exception/BadRequestException.cs
@@ -0,0 +1,20 @@
+﻿using FluentValidation.Results;
+
+namespace HR.LeaveManagement.Application.Exception;
+
+public class BadRequestException : System.Exception
+{
+    public BadRequestException(string message) : base(message)
+    {
+    }
+
+    public BadRequestException(string message, ValidationResult validationResult) : base(message)
+    {
+        ValidationErrors = new();
+
+        foreach (var error in validationResult.Errors)
+            ValidationErrors.Add(error.ErrorMessage);
+    }
+
+    public List<string> ValidationErrors { get; set; }
+}
