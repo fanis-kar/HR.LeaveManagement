be2352cc0ace1c3240eac4dee7b8af54f3b160fc
Theofanis Karamichalelis <fanis.karamichalelis@gmail.com>
2025-01-19 00:38:55 +0200

    Add cqrs commands

---
+++ HR.LeaveManagement.Application/Features/LeaveType/Commands/DeleteLeaveType/DeleteLeaveTypeCommandHandler.cs
@@ -0,0 +1,29 @@
+﻿using HR.LeaveManagement.Application.Contracts.Persistence;
+using MediatR;
+
+namespace HR.LeaveManagement.Application.Features.LeaveType.Commands.DeleteLeaveType;
+
+public class DeleteLeaveTypeCommandHandler : IRequestHandler<DeleteLeaveTypeCommand, Unit>
+{
+    private readonly ILeaveTypeRepository _leaveTypeRepository;
+
+    public DeleteLeaveTypeCommandHandler(ILeaveTypeRepository leaveTypeRepository)
+    {
+        _leaveTypeRepository = leaveTypeRepository;
+    }
+
+    public async Task<Unit> Handle(DeleteLeaveTypeCommand request, CancellationToken cancellationToken)
+    {
+        // Retrieve domain entity object
+        var leaveTypeToDelete = await _leaveTypeRepository.GetByIdAsync(request.Id);
+
+        // Verify that record exists
+
+
+        // Remove from database
+        await _leaveTypeRepository.DeleteAsync(leaveTypeToDelete);
+
+        // Return record Id
+        return Unit.Value;
+    }
+}
