bcb7922274f8133f4d8d8eb207d9249c0510cbe6
Theofanis Karamichalelis <fanis.karamichalelis@gmail.com>
2025-01-21 00:02:40 +0200

    Add feature for Leave Allocation and Request Add Middleware Add API

--- HR.LeaveManagement.Infrastructure/InfrastructureServicesRegistration.cs
+++ HR.LeaveManagement.Infrastructure/InfrastructureServicesRegistration.cs
@@ -10,7 +10,7 @@ namespace HR.LeaveManagement.Infrastructure;
 
 public static class InfrastructureServicesRegistration
 {
-    public static IServiceCollection ConfigureInfrastructureServices(this IServiceCollection services, IConfiguration configuration)
+    public static IServiceCollection AddInfrastructureServices(this IServiceCollection services, IConfiguration configuration)
     {
         services.Configure<EmailSettings>(configuration.GetSection("EmailSettings"));
         services.AddTransient<IEmailSender, EmailSender>(); // i want to create a new instance it's time I use it
